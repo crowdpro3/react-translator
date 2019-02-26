7830383b0e5f5a15fa4cf3c4159e86c2b237d3f2
Kurzdor <pavloeksmidt@gmail.com>
2019-02-27 01:51:55 +0200

    Added context, output from context, some refactoring

--- src/components/Controls/LangControls.js
+++ src/components/Controls/LangControls.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
+import { TranslatorContext } from '../../utils'
 import { LangSelector } from './LangSelector'
 import { LangSwitcher } from './LangSwitcher'
+
+const LangControls = () => {
+  return (
+    <TranslatorContext.Consumer>
+      {value => (
+        <LangControlsContainer>
+          <LangSelector>{value.state.langFrom}</LangSelector>
+          <LangSwitcher />
+          <LangSelector>{value.state.langTo}</LangSelector>
+        </LangControlsContainer>
+      )}
+    </TranslatorContext.Consumer>
+  )
+}
 
 const LangControlsContainer = styled.div`
   display: flex;
 `
-
-const LangControls = () => {
-  return (
-    <LangControlsContainer>
-      <LangSelector>английский</LangSelector>
-      <LangSwitcher />
-      <LangSelector>русский</LangSelector>
-    </LangControlsContainer>
-  )
-}
 
 export default LangControls
