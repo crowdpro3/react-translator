ddf02ffa0384610363aaa4aef209d6b05d9c4135
Kurzdor <pavloeksmidt@gmail.com>
2019-02-28 00:33:25 +0200

    Move to global state

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,39 @@ class App extends Component {
   state = {
     langFrom: 'английский',
     langTo: 'русский',
+    textAreaValue: '',
+    symbolsCount: 0,
+    maxSymbolsCount: 200,
+    isEmpty: true,
+  }
+
+  resetTextArea = () => {
+    this.setState(() => {
+      return {
+        textAreaValue: '',
+        symbolsCount: 0,
+        isEmpty: true,
+      }
+    })
+  }
+
+  onTextAreaInput = event => {
+    const { maxSymbolsCount } = this.state
+    const textAreaValue = event.target.value
+    const symbolsCount = textAreaValue.length
+    let isEmpty = textAreaValue === ''
+
+    if (symbolsCount > maxSymbolsCount) {
+      return false
+    }
+
+    this.setState(() => {
+      return {
+        textAreaValue,
+        symbolsCount,
+        isEmpty,
+      }
+    })
   }
 
   swapLangs = () => {
@@ -27,6 +60,9 @@ class App extends Component {
           state: this.state,
           actions: {
             swapLangs: this.swapLangs,
+            getUserInput: this.getUserInput,
+            resetTextArea: this.resetTextArea,
+            onTextAreaInput: this.onTextAreaInput,
           },
         }}
       >
